f1b24fabb7e2dab821bcebd4767e5d73f028a28f
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-05-20 17:06:17 +0300

    Initial Commit

---
+++ Audio player/Audio_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_playerApp.swift
+//  Audio player
+//
+//  Created by Ruslan Usmanov on 2023-05-20.
+//
+
+import SwiftUI
+
+@main
+struct Audio_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audio player/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Audio player
+//
+//  Created by Ruslan Usmanov on 2023-05-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
