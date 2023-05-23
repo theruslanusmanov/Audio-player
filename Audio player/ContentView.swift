b8d8e07ba0736f63cf6512dd24928b7cb4284ed6
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-05-23 12:03:27 +0300

    update layout

--- Audio player/ContentView.swift
+++ Audio player/ContentView.swift
@@ -12,18 +12,56 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image(systemName: "chevron.down")
                 }
+                Spacer()
             }
-            
             Image("CoverPicture")
                 .resizable()
                 .frame(width: 300, height: 300)
             
-            Text("Hello, world!")
+            Text("What To Do (&Me Remix)")
+            Text("Guy Gerber")
+            Spacer()
+            HStack {
+                Button {
+                    print("PLAY")
+                } label: {
+                    Image(systemName: "text.line.first.and.arrowtriangle.forward")
+                }
+                Button {
+                    print("PLAY")
+                } label: {
+                    Image(systemName: "backward.end.fill")
+                }
+                Button {
+                    print("PLAY")
+                } label: {
+                    Image(systemName: "play.fill")
+                }
+                .foregroundColor(.black)
+                Button {
+                    print("PLAY")
+                } label: {
+                    Image(systemName: "forward.end.fill")
+                }
+                Button {
+                    print("PLAY")
+                } label: {
+                    Image(systemName: "ellipsis")
+                }
+            }
+            
+            .frame(maxWidth: .infinity)
+            .frame(height: 75)
+            .background(Color("AccentColor"))
+            .foregroundColor(.black)
         }
-        .padding()
+        .padding(.top, 75)
+        .ignoresSafeArea()
     }
 }
 
